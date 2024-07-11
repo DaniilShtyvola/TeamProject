e5896dd5ec9968bcbd7ba32a874c2e91c94d7cc0
ProBAN <87663703+ProBANgh@users.noreply.github.com>
2024-07-11 10:04:49 +0300

    Add region seaarch

---
+++ src/components/PlateCard/PlateCard.styled.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import { Card } from 'react-bootstrap';
+
+export const StyledCard = styled(Card)`
+   width: 100%;
+   margin-bottom: 20px; /* Пример отступа между карточками */
+`;
